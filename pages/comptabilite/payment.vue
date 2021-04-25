2b379009dbf64347f7d3c5c1071e26032acd1e64
leenorshn <katembovictor@gmail.com>
2021-04-25 11:10:13 +0100

    redesigning button for payment

--- pages/comptabilite/payment.vue
+++ pages/comptabilite/payment.vue
@@ -199,12 +199,18 @@
                   </div>
                 </div>
               </div>
-              <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <div class="px-4 py-3 bg-gray-50 text-right sm:px-6 flex space-x-4 items-center justify-end">
+                  <button
+                  type="submit"
+                  class="inline-flex justify-center py-2 px-20 border-2 border-indigo-500 shadow-sm text-sm font-medium rounded-md text-indigo-700  bg-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Annuler
+                </button>
                 <button
                   type="submit"
-                  class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  class="inline-flex justify-center py-2 px-20 border-2 border-indigo-500 shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Save
+                  Confirmer
                 </button>
               </div>
             </div>

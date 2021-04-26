f524d25881e2068e1a10bbed63d4dfd26bc4280c
leenorshn <katembovictor@gmail.com>
2021-04-26 08:17:14 +0100

    start working on users menus

---
+++ components/AccountItem.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="flex items-end space-x-3">
+      <div class="h-20 w-4/5 border border-gray-200 rounded-md flex p-2 space-x-4">
+        <div class="flex-shrink-0">
+          <div class="w-16 h-16 rounded-full bg-indigo-100"></div>
+        </div>
+        <div class="flex justify-between w-full">
+          <div class="">
+            <h3 class="text-blue-700 text-xl tracking-tight font-semibold">
+              Account name
+            </h3>
+            <p class="text-sm text-gray-500">
+              Account Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            </p>
+          </div>
+          <h4>#001-A</h4>
+        </div>
+      </div>
+      <div class="space-y-2">
+        <button to="/" class="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5 text-gray-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+          <span class="text-blue-500 text-sm">Effacer</span>
+        </button>
+        <nuxt-link to="/" class="flex text-gray-500">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+            />
+          </svg>
+          <span class="text-blue-500 text-sm">editer</span>
+        </nuxt-link>
+      </div>
+    </div>
+</template>
--- pages/comptabilite/account.vue
+++ pages/comptabilite/account.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>
-        account
+  <div class="p-4">
+    <div class="flex items-center justify-end">
+      <nuxt-link to="/" class="bg-indigo-900 text-white px-8 py-2 rounded-md"
+        >Nouveau</nuxt-link
+      >
     </div>
+    <account-item v-for="(n,i) in 5" :key="i" class="mb-2"/>
+  </div>
 </template>
 <script>
 export default {
-    layout:"account"
-}
+  layout: "account",
+};
 </script>

0a4a25dae0a30e047a4df75c7dc3fa7477cbccc3
leenorshn <katembovictor@gmail.com>
2021-04-30 09:31:34 +0100

    trying to authenticate

---
+++ store/index.js
@@ -0,0 +1,18 @@
+export const state = () => ({
+    counter: 0
+  })
+  
+  export const mutations = {
+    increment(state) {
+      state.counter++
+    }
+  }
+  
+  export const actions={
+      login(context,data){
+
+      },
+      register(context,data){
+
+      }
+  }

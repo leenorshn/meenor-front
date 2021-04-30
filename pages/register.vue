cd2083148a1f71e08d83d3cef7cdc7d87f2616b5
leenorshn <katembovictor@gmail.com>
2021-04-30 16:19:41 +0100

    adding some notification error

--- pages/register.vue
+++ pages/register.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="flex h-screen w-screen">
-    <div class="w-2/5 flex items-center px-8 justify-center bg-blue-800 relative">
-        <div class="">
-          <h2 class="text-center text-7xl text-gray-50 mt-6">Meenor</h2>
+    <div
+      class="w-2/5 flex items-center px-8 justify-center bg-blue-800 relative"
+    >
+      <div class="">
+        <h2 class="text-center text-7xl text-gray-50 mt-6">Meenor</h2>
         <p class="text-center text-xl text-gray-200 mt-6">
-            Creer votre compte pour gerer en toute liberte vos multiples revenus locatif,
-            d'une facon tres facile.
-            Soyez au controle de vos revenus locatifs</p>
-        </div>
+          Creer votre compte pour gerer en toute liberte vos multiples revenus
+          locatif, d'une facon tres facile. Soyez au controle de vos revenus
+          locatifs
+        </p>
+      </div>
     </div>
     <div class="flex-1 bg-gray-200">
-      <div class="px-24 mx-auto my-auto py-14">
+      <div class="px-24 mx-auto my-auto py-14 relative">
         <h2 class="text-blue-800 text-3xl font-semibold">Creer un compte</h2>
         <form>
-          <div  class="mt-4 w-2/3">
+          <div class="mt-4 w-2/3">
             <label for="entrepriseName" class="text-gray-500 mb-2"
               >Votre entreprise:</label
             >
@@ -25,11 +28,10 @@
               class="w-full rounded-md border mt-1 border-gray-100 placeholder-gray-400 focus:border-gray-200 outline-none"
             />
           </div>
-          
 
-          <div class="flex  justify-between space-x-2 mt-4">
+          <div class="flex justify-between space-x-2 mt-4">
             <div class="w-2/3">
-              <label for="entrepriseCity" class=" text-gray-500 mb-2"
+              <label for="entrepriseCity" class="text-gray-500 mb-2"
                 >Votre ville:</label
               >
               <input
@@ -37,13 +39,13 @@
                 type="text"
                 v-model="company.address.city"
                 placeholder="Ex: Butembo"
-                class="w-full rounded-md mt-1 border  border-gray-100 placeholder-gray-400 focus:border-gray-200 outline-none"
+                class="w-full rounded-md mt-1 border border-gray-100 placeholder-gray-400 focus:border-gray-200 outline-none"
               />
             </div>
           </div>
           <div class="flex justify-between space-x-2 mt-4">
             <div class="w-2/3">
-              <label for="entreprisePhone" class=" text-gray-500 mb-2"
+              <label for="entreprisePhone" class="text-gray-500 mb-2"
                 >Numero de telephone:</label
               >
               <input
@@ -51,10 +53,9 @@
                 type="text"
                 v-model="company.phone"
                 placeholder="Numero de telephone"
-                class="w-full rounded-md mt-1 border  border-gray-100 placeholder-gray-400 focus:border-gray-200 outline-none"
+                class="w-full rounded-md mt-1 border border-gray-100 placeholder-gray-400 focus:border-gray-200 outline-none"
               />
             </div>
-            
           </div>
           <div class="mt-4 w-2/3">
             <label for="entreprisePassword" class="text-gray-500 mb-2"
@@ -69,13 +70,25 @@
             />
           </div>
           <div class="mt-4">
-            <input type="checkbox"  id="user-case" class=""/>
-            <span>Acceptez les <a class="text-blue-500 underline" href="#"> conditions</a> d'utilisation</span>
+            <input type="checkbox" id="user-case" class="" />
+            <span
+              >Acceptez les
+              <a class="text-blue-500 underline" href="#"> conditions</a>
+              d'utilisation</span
+            >
           </div>
           <div class="flex justify-center mt-8">
-            <button @click.prevent="registerCompany()" class="px-16 py-4 bg-blue-800  text-white font-bold text-sm uppercase rounded-md">Creer compte</button>
+            <button
+              @click.prevent="registerCompany()"
+              class="px-16 py-4 bg-blue-800 text-white font-bold text-sm uppercase rounded-md"
+            >
+              Creer compte
+            </button>
           </div>
         </form>
+        <div class="h-16 w-6/12 p-4 text-white bg-red-500 rounded-md absolute right-3 top-3">
+            {{error}} 
+        </div>
       </div>
     </div>
   </div>
@@ -84,29 +97,40 @@
 <script>
 import { mapActions } from "vuex";
 export default {
-    name:"Register",
-    layout:'auth',
-    data(){
-        return {
-            company:{
-                address:{
-                    city:'',
-                    local:"Aucun"
-                }
-            }
-        }
+  name: "Register",
+  layout: "auth",
+  data() {
+    return {
+      company: {
+        address: {
+          city: "",
+          local: "Aucun",
+        },
+      },
+      error:""
+    };
+  },
+  methods: {
+    ...mapActions(["register"]),
+    registerCompany() {
+      if (
+        this.company.name &&
+        this.company.address.city &&
+        this.company.phone &&
+        this.company.password
+      ) {
+        this.register(this.company)
+          .then((d) => {
+            this.$router.push("/login");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }else{
+          this.error="Error veillez remplir le formulaire correctement."
+      }
     },
-    methods:{
-        ...mapActions(['register']),
-        registerCompany(){
-            this.register(this.company).
-            then(d=>{
-                this.$router.push('/login');
-            }).catch(err=>{
-                console.log(err);
-            });
-        }
-    }
+  },
 };
 </script>
 

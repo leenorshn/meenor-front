64f7ab83557c6b19e84b26633a6d13368c2149b5
leenorshn <katembovictor@gmail.com>
2021-05-10 18:41:12 +0100

    implementing createPayment method in payment actions

--- store/payment.js
+++ store/payment.js
@@ -1,8 +1,27 @@
+import { NEW_PAYMENT } from "../apollo/payment_gql";
 
 export const state = () => ({
-    payments:[]
+  payments: []
 });
 
-export const mutations = {
-    
+export const mutations = {};
+
+export const actions = {
+  async createPayment(_,data) {
+    let client = this.app.apolloProvider.defaultClient;
+    try {
+
+        const res = await client
+        .mutate({ mutation: NEW_PAYMENT, variables: {data} })
+        .then(({ data }) => {
+          console.log(data);
+          return data && data.createPayment;
+        });
+
+      console.log(res);
+
+    } catch (error) {
+        console.log(error);
+    }
+  }
 };

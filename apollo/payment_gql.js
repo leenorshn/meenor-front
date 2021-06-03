ee87c8b65073839c211d7736776caf5d6638aacb
leenorshn <katembovictor@gmail.com>
2021-06-03 11:03:31 +0100

    cleanging code

--- apollo/payment_gql.js
+++ apollo/payment_gql.js
@@ -4,18 +4,12 @@ export const GET_PAYMENTS = gql`
   query getpayments{
     payments {
       id
-      locataire {
-        id
-        name
-        avatar
-        phone
-      }
-      room {
-        id
-        format
-        numero
-        price
-      }
+      # locataire {
+      #   id
+      #   name
+      #   avatar
+      #   phone
+      # }
       amount
       fromDate
       toDate

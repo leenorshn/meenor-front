933a59362d46c92858d9fe54613fdd10282d36d7
leenorshn <katembovictor@gmail.com>
2021-06-18 20:31:38 +0200

    done

--- components/ChambreItem.vue
+++ components/ChambreItem.vue
@@ -74,6 +74,31 @@
           hover:text-blue-700
           rounded-full
         "
+        to=""
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 "
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+          />
+        </svg>
+      </nuxt-link>
+      <nuxt-link
+        class="
+          p-2
+          hover:bg-blue-300
+          text-gray-400
+          hover:text-blue-700
+          rounded-full
+        "
         :to="`/maison/${chambre.id}/room`"
       >
         <svg
@@ -97,7 +122,8 @@
           />
         </svg>
       </nuxt-link>
-      <button class="
+      <button
+        class="
           p-2
           hover:bg-red-300
           text-gray-400
@@ -105,7 +131,8 @@
           rounded-full
           focus:ring-0
           ring-0
-        ">
+        "
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-5 w-5 text-gray-400"
@@ -134,10 +161,10 @@ export default {
   data() {
     return {};
   },
-  methods:{
-    deleteOneRoom(){
-      alert(`chambre:${this.chambre.id}`)
-    }
-  }
+  methods: {
+    deleteOneRoom() {
+      //this.alert(`chambre:${this.chambre.id}`)
+    },
+  },
 };
 </script>

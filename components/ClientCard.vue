0d93d6fcf70fd9845dc901fa7739884fbf10928b
leenorshn <katembovictor@gmail.com>
2021-06-18 21:02:41 +0200

    adding others button on the cleint page

--- components/ClientCard.vue
+++ components/ClientCard.vue
@@ -3,33 +3,57 @@
     <td class="px-6 py-4 whitespace-nowrap">
       <div class="flex items-center">
         <div class="flex-shrink-0 h-10 w-10">
-          <img
-            class="h-10 w-10 rounded-full"
-            src="/profile.png"
-            alt=""
-          />
+          <img class="h-10 w-10 rounded-full" src="/profile.png" alt="" />
         </div>
         <div class="ml-4">
-          <div class="text-sm font-medium text-gray-900">{{client.name}}</div>
-          <div class="text-sm text-gray-400">{{client.phone}}</div>
+          <div class="text-sm font-medium text-gray-900">{{ client.name }}</div>
+          <div class="text-sm text-gray-400">{{ client.phone }}</div>
         </div>
       </div>
     </td>
     <td class="px-6 py-4 whitespace-nowrap">
-      <div class="text-sm text-gray-900">Chambre n-{{client.room?client.room.numero:""}}</div>
+      <div class="text-sm text-gray-900">
+        Chambre n-{{ client.room ? client.room.numero : "" }}
+      </div>
     </td>
     <td class="px-6 py-4 whitespace-nowrap">
       <span
-        class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+        class="
+          px-2
+          inline-flex
+          text-xs
+          leading-5
+          font-semibold
+          rounded-full
+          bg-green-100
+          text-green-800
+        "
       >
         Active
       </span>
     </td>
     <td
-      class="px-6 py-4 flex justify-center items-center whitespace-nowrap text-sm text-gray-500"
+      class="
+        px-6
+        py-4
+        flex
+        justify-center
+        items-center
+        whitespace-nowrap
+        text-sm text-gray-500
+      "
     >
       <button
-        class="w-9 h-9 flex justify-center items-center rounded-full hover:bg-blue-200 hover:text-blue-600"
+        class="
+          w-9
+          h-9
+          flex
+          justify-center
+          items-center
+          rounded-full
+          hover:bg-blue-200
+          hover:text-blue-600
+        "
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -48,30 +72,28 @@
       </button>
     </td>
     <td
-      class="px-6 py-2 text-gray-500 whitespace-nowrap text-right text-sm font-medium"
+      class="
+        px-6
+        py-2
+        text-gray-500
+        whitespace-nowrap
+        text-right text-sm
+        font-medium
+      "
     >
       <div class="flex justify-center">
         <button
-          class="w-9 h-9 flex justify-center items-center rounded-full hover:bg-blue-200 hover:text-blue-600"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-            />
-          </svg>
-        </button>
-        <button
           to="/"
-          class="w-9 h-9 flex justify-center items-center rounded-full hover:bg-blue-200 hover:text-blue-600"
+          class="
+            w-9
+            h-9
+            flex
+            justify-center
+            items-center
+            rounded-full
+            hover:bg-orange-200
+            hover:text-black
+          "
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -88,6 +110,64 @@
             />
           </svg>
         </button>
+        <nuxt-link :to="`/client/${client.id}`" class="
+            w-9
+            h-9
+            flex
+            justify-center
+            items-center
+            rounded-full
+            hover:bg-blue-200
+            hover:text-blue-600">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+            />
+          </svg>
+        </nuxt-link>
+        <button
+          class="
+            w-9
+            h-9
+            flex
+            justify-center
+            items-center
+            rounded-full
+            hover:bg-red-200
+            hover:text-red-700
+          "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+        </button>
+        
       </div>
     </td>
   </tr>
@@ -95,8 +175,8 @@
 
 <script>
 export default {
-  props:{
-    client:Object
-  }
-}
+  props: {
+    client: Object,
+  },
+};
 </script>

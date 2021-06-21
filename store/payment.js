ca29b56e6396b37d2fdff8386e46131d3b2dd679
leenorshn <katembovictor@gmail.com>
2021-06-21 09:47:53 +0200

    update payment to support notification

--- store/payment.js
+++ store/payment.js
@@ -19,12 +19,20 @@ export const actions = {
         });
 
       console.log(res);
+      dispatch("pushNotification",{
+        type:"done",
+        message:"Payment creer!"
+      },{root:true})
 
     } catch (error) {
         console.log(error);
+        dispatch("pushNotification",{
+          type:"error",
+          message:"Erreur de payment!"
+        },{root:true})
     }
   },
-  async createSortie(_,data){
+  async createSortie({dispatch},data){
     let client =this.app.apolloProvider.defaultClient;
     try {
       const res= client.mutate({
@@ -34,8 +42,16 @@ export const actions = {
         return data && data.createSortie;
       });
       console.log(res);
+      dispatch("pushNotification",{
+        type:"done",
+        message:" Sortie creer!"
+      },{root:true})
     } catch (error) {
       console.log(error);
+      dispatch("pushNotification",{
+        type:"error",
+        message:"Erreur de creation de sortie"
+      },{root:true})
     }
   }
 };

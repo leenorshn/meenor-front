2c005d6a3161ebb123e9086c8d8250088b3abc77
leenorshn <katembovictor@gmail.com>
2021-06-22 15:24:54 +0200

    adding general

--- pages/settings/index/general.vue
+++ pages/settings/index/general.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        general
+  <div>
+    <div class="flex justify-between">
+      <h2 class="text-xl text-blue-800">General</h2>
+      <nuxt-link
+        to="/settings/general/editor"
+        class="rounded-md bg-blue-800 text-white px-5 py-2 hover:bg-blue-400"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+          />
+        </svg>
+      </nuxt-link>
     </div>
+  </div>
 </template>

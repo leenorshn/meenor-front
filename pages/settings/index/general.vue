97fa522978a25dd7066304b3a46077e7c9d3a571
leenorshn <katembovictor@gmail.com>
2021-06-23 09:50:54 +0200

    updating general

--- pages/settings/index/general.vue
+++ pages/settings/index/general.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="flex justify-between">
-      <h2 class="text-xl text-blue-800">General</h2>
+      <h2 class="text-xl font-bold text-blue-800">General</h2>
       <nuxt-link
         to="/settings/general/editor"
         class="rounded-md bg-blue-800 text-white px-5 py-2 hover:bg-blue-400"
@@ -21,6 +21,41 @@
           />
         </svg>
       </nuxt-link>
+    </div>
+    <div class="space-y-8">
+      <div class="flex items-center space-x-6">
+        <div class="font-semibold text-black text-lg">Nomination:</div>
+        <div>Ets Mulumbi Ranga</div>
+      </div>
+      <div class="flex items-center space-x-16">
+        <div class="font-semibold text-black text-lg">RCCM:</div>
+        <div>_ _ _ _ _ _ _ _ _ _ _ _ _ _</div>
+      </div>
+      <div class="flex items-center space-x-16">
+        <div class="font-semibold text-black text-lg">Id-Nat:</div>
+        <div>_ _ _ _ _ _ _ _ _ _ _ _ _ _</div>
+      </div>
+      <div>
+        <div class="font-semibold text-lg">Adresse:</div>
+        <div class="text-gray-500">
+          Butembo/ Rue president/Rond point OASIS/Batiment Hauje
+        </div>
+      </div>
+      <div>
+        <div class="font-semibold text-black text-lg">Contacts:</div>
+        <div class="ml-4">
+          <h2 class="font-semibold text-gray-700 text-lg">
+            Email:   <span class="ml-4 text-sm font-normal text-gray-600">real@gmail.com</span>
+          </h2>
+          <h2 class="font-semibold text-gray-700 text-lg">
+            Phone: <span class="ml-2 text-sm font-normal text-gray-600">+2439781543 _ _</span>
+          </h2>
+        </div>
+      </div>
+      <div>
+        <div class="font-semibold text-black text-lg">Detail:</div>
+        <h4 class="text-gray-500">Entreprise de gestion de location</h4>
+      </div>
     </div>
   </div>
 </template>
